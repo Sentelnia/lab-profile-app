3c244bd29411f0b428d906ee12e5969f46c1842e
Mathilde Tomba <mathilde.tomba@gmail.com>
2021-07-10 13:01:36 +0200

    it3

--- lab-profil-app/profile-app/src/Component/Home.js
+++ lab-profil-app/profile-app/src/Component/Home.js
@@ -5,8 +5,8 @@ class Home extends Component{
     render(){
       return(
         <div>
-        <Link to='/signup'>Signup</Link>
-        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup </Link>
+        <Link to='/login'> Login</Link>
         </div>
       )
     }
--- lab-profil-app/profile-app/src/Component/Signup.js
+++ lab-profil-app/profile-app/src/Component/Signup.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import {signup} from './auth/auth-service';
+import { Link } from 'react-router-dom';
 
 class Signup extends Component{
+
+    state = { username: '', password: '' }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        const username = this.state.username;
+        const password = this.state.password;
+     
+        signup(username, password)
+          .then(response => {
+            this.setState({username: "", password: ""});
+            this.props.updateUser(response)
+          })
+          .catch(error => console.log(error))
+      }
+     
+      // HERE
+      handleChange = (event) => {  
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+      }
+
     render(){
       return(
         <div>
-         signup
+          <form onSubmit={this.handleFormSubmit}>
+          <label>Username:</label>
+          <input type="text" name="username" value={this.state.username} onChange={e => this.handleChange(e)} />
+          
+          <label>Password:</label>
+          <input name="password" value={this.state.password} onChange={e => this.handleChange(e)} />
+          
+          <button>I Signup</button>
+        </form>
+ 
+        <p>Already have account? 
+          <Link to={"/login"}>Login</Link>
+        </p>
+ 
         </div>
       )
     }
